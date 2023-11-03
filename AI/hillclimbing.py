df08ba429bcc97df27cbd8db9586cd14677182e7
PrerakPatel13 <prerakpatel1301@gmail.com>
2023-11-03 23:05:12 +0530

    MODIFIED

--- AI/hillclimbing.py
+++ AI/hillclimbing.py
@@ -1,8 +1,8 @@
 import copy
-visited = []
-def gn(current,prev_heu,goal):
-    global visited
-    state = copy.deepcopy(current)
+visited_states = []
+def gn(curr_state,prev_heu,goal):
+    global visited_states
+    state = copy.deepcopy(curr_state)
     for i in range(len(state)):
         temp = copy.deepcopy(state)
         if len(temp[i]) > 0:
@@ -11,104 +11,47 @@ def gn(current,prev_heu,goal):
                 temp1 = copy.deepcopy(temp)
                 if j != i:
                     temp1[j] = temp1[j] + [elem]
-                    if (temp1 not in visited):
-                        curr_heu=heuristic(temp1,goal)
-                        if curr_heu>prev_heu:
-                            child = copy.deepcopy(temp1)
-                            return child
- 
+                    curr_heu=heuristic(temp1,goal)
+                    if curr_heu>prev_heu:
+                        child = copy.deepcopy(temp1)
+                        return child
     return 0
-def heuristic(current,goal):
-    goalc=goal[3]
+
+def heuristic(curr_state,goal):
+    copygoal=goal[3]
     val=0
-    for i in range(len(current)):
-        check_val=current[i]
+    for i in range(len(curr_state)):
+        check_val=curr_state[i]
         if len(check_val)>0:
             for j in range(len(check_val)):
-                if check_val[j]!=goalc[j]:
+                if check_val[j]!=copygoal[j]:
                     val-=j
                 else:
                     val+=j
+    print(f"Heuristic value for {curr_state} is {val}")
     return val
  
 def sln(init_state,goal):
-    global visited
+    global visited_states
     if (init_state == goal):
-        print (goal)
-        print("solution found!")
+        print(f"Solution found! {goal}\n")
         return
-    current_state = copy.deepcopy(init_state)
+    current = copy.deepcopy(init_state)
  
     while(True):
-        visited.append(copy.deepcopy(current_state))
-        print(current_state)
-        prev_heu=heuristic(current_state,goal)
-        child = gn(current_state,prev_heu,goal)
+        visited_states.append(copy.deepcopy(current))
+        print(f"Current State : {current}")
+        prev_heu=heuristic(current,goal)
+        child = gn(current,prev_heu,goal)
+        
         if child==0:
-            print("Final state - ",current_state)
+            print(f"No heuristic value is obtained better than this thus declaring this as goal state - {current}\n")
             return
- 
-        current_state = copy.deepcopy(child)
+        print(f"Child chosen for exploration : {child}\n")        
+        current = copy.deepcopy(child)
  
 def main():
-    global visited
-    initial = [[],[],[],['B','C','D','A']]
-    goal = [[],[],[],['A','B','C','D']]
-    sln(initial,goal)
-import copy
-visited = []
-def gn(current,prev_heu,goal):
-    global visited
-    state = copy.deepcopy(current)
-    for i in range(len(state)):
-        temp = copy.deepcopy(state)
-        if len(temp[i]) > 0:
-            elem = temp[i].pop()
-            for j in range(len(temp)):
-                temp1 = copy.deepcopy(temp)
-                if j != i:
-                    temp1[j] = temp1[j] + [elem]
-                    if (temp1 not in visited):
-                        curr_heu=heuristic(temp1,goal)
-                        if curr_heu>prev_heu:
-                            child = copy.deepcopy(temp1)
-                            return child
- 
-    return 0
-def heuristic(current,goal):
-    goalc=goal[3]
-    val=0
-    for i in range(len(current)):
-        check_val=current[i]
-        if len(check_val)>0:
-            for j in range(len(check_val)):
-                if check_val[j]!=goalc[j]:
-                    val-=j
-                else:
-                    val+=j
-    return val
- 
-def sln(init_state,goal):
-    global visited
-    if (init_state == goal):
-        print (goal)
-        print("solution found!")
-        return
-    current_state = copy.deepcopy(init_state)
- 
-    while(True):
-        visited.append(copy.deepcopy(current_state))
-        print(current_state)
-        prev_heu=heuristic(current_state,goal)
-        child = gn(current_state,prev_heu,goal)
-        if child==0:
-            print("Final state - ",current_state)
-            return
- 
-        current_state = copy.deepcopy(child)
- 
-def main():
-    global visited
+    global visited_states
     initial = [[],[],[],['B','C','D','A']]
     goal = [[],[],[],['A','B','C','D']]
     sln(initial,goal)
