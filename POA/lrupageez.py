deb52be70c32c2dc5e15c4dff8a887e1a326c035
PrerakPatel13 <prerakpatel1301@gmail.com>
2023-12-13 00:36:09 +0530

    modify

--- POA/lrupageez.py
+++ POA/lrupageez.py
@@ -1,15 +1,15 @@
 from collections import OrderedDict
 
 class LRUPageReplacement:
-    capacity, page_order = 3, OrderedDict()
+    capacity, page_queue = 3, OrderedDict()
 
-    def check_page_fault(self, page):
-        if page in self.page_order:
-            self.page_order.move_to_end(page)
+    def page_fault(self, page):
+        if page in self.page_queue:
+            self.page_queue.move_to_end(page)
             return False
-        elif len(self.page_order) == self.capacity:
-            self.page_order.popitem(last=False)
-        self.page_order[page] = None
+        elif len(self.page_queue) == self.capacity:
+            self.page_queue.popitem(last=False)
+        self.page_queue[page] = None
         return True
 
 lru_algorithm = LRUPageReplacement()
@@ -17,7 +17,7 @@ lru_algorithm = LRUPageReplacement()
 page_requests = [1, 2, 3, 4, 1, 2, 5, 1, 2]
 
 for page in page_requests:
-    if lru_algorithm.check_page_fault(page):
-        print(f"Page {page} caused a page fault. Page order: {list(lru_algorithm.page_order.keys())}")
+    if lru_algorithm.page_fault(page):
+        print(f"Page {page} caused a page fault. Page order: {list(lru_algorithm.page_queue.keys())}")
     else:
-        print(f"Page {page} is already in the memory HIT . Page order: {list(lru_algorithm.page_order.keys())}")
+        print(f"Page {page} is already in the memory HIT . Page order: {list(lru_algorithm.page_queue.keys())}")
