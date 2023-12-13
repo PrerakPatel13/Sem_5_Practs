60ce64e72c24e2ab5301d9bd7c99f685ef5a1073
PrerakPatel13 <prerakpatel1301@gmail.com>
2023-12-13 23:53:08 +0530

    modify

--- AI/astar.py
+++ AI/astar.py
@@ -1,64 +1,74 @@
+# Define the graph
 graph = {
-    'A': {'B': 6, 'F': 3},
-    'B': {'C': 3,'D': 2},
-    'C': {'D': 1,'E': 5},
-    'D': {'E': 8},
-    'E': {'I': 5,'J': 5},
-    'F': {'G': 1,'H': 7},
-    'G': {'I': 3},
-    'H': {'I': 2},
-    'I': {'J': 3},
-    'J': {}
+    'S': {'A': 5, 'B': 8, 'C': 12},
+    'A': {'D': 6, 'E': 9},
+    'B': {'F': 10},
+    'C': {'G': 11},
+    'D': {'H': 8, 'I': 15},
+    'E': {'J': 7},
+    'F': {'K': 12},
+    'G': {'L': 9},
+    'H': {'M': 10},
+    'I': {'N': 5},
+    'J': {'N': 8},
+    'K': {'N': 6},
+    'L': {'N': 10},
+    'M': {'N': 7},
+    'N': {'Z': 0},
+    'Z': {}
 }
-
-heuristic = {
-    'A': 10,
-    'B': 8,
-    'C': 5,
-    'D': 7,
-    'E': 3,
-    'F': 6,
-    'G': 5,
-    'H': 3,
-    'I': 1,
-    'J': 0,
+# Define the heuristic values for A* search
+graph_heu = {
+    'S': 20,
+    'A': 16,
+    'B': 18,
+    'C': 15,
+    'D': 12,
+    'E': 10,
+    'F': 8,
+    'G': 6,
+    'H': 6,
+    'I': 5,
+    'J': 4,
+    'K': 3,
+    'L': 2,
+    'M': 1,
+    'N': 0,
+    'Z': 0
 }
-
-
-current = "A"
-goal = "J"
-
-def searchlist(l,k):
-    for i in l:
-        if i[0] == k:
-            return i
-
-def funcofn(gl,cn): 
+curr_node = input("Enter Start Node : ") # this is also start node at the beginning
+goal_node = input("Enter Goal Node : ")
+def funcofn(gl,cn): # gl <- path of previous node , cn <- current node
     g =0
+    print(gl,cn) 
     for i in range(len(gl)-1):
         g += graph[gl[i]][gl[i+1]]
-    g += graph[gl[-1]][cn] + heuristic[cn]
+    g += graph[gl[-1]][cn] + graph_heu[cn] #graph['S']['A'] isme pehle graph['S'] execute hoga fir voh key se A ka value milega i.e. 5
     return g
-
-open_list = []
+open_list = []    
 close_list = []
-open_list.append([current,heuristic[current],[current]])
-n = 0 
+open_list.append([curr_node,graph_heu[curr_node],[curr_node]])
 try:
     while True:
-        parent = open_list[0][0]
-        close_list.append(parent)
+        parent_node = open_list[0][0]
+        # print(parent_node)
+        close_list.append(parent_node)
+        print(open_list)
         current_path = open_list[0][2]
-        if parent == goal:
+        if parent_node == goal_node:
             break
-        print(f"Currently at node : {parent}")
+        print(f"Currently at node : {parent_node}")
         for j in graph[open_list[0][0]]:
             open_list.append([j,funcofn(current_path,j),current_path+[j]])
             open_list = sorted(open_list, key=lambda x: x[1])
-        open_list = [pair for pair in open_list if pair[0] != parent]
-        n+=1   
+        # open_list = [pair for pair in open_list if pair[0] != parent_node]
+        # before:[['S', 20, ['S']], ['A', 21, ['S', 'A']], ['B', 26, ['S', 'B']], ['C', 27, ['S', 'C']]]
+        #basically ye loop se parent hata diya open list se taaki next iteration mein S ki jagah A lenge as parent
+        for i in open_list:
+            if i[0] == parent_node:
+                open_list.remove(i)
+        # after:[['A', 21, ['S', 'A']], ['B', 26, ['S', 'B']], ['C', 27, ['S', 'C']]]  
         print(f"Open List is : {open_list}\nclosed List is : {close_list}\n")
 except:
-    print("Node not Found")        
-
-print(f"Solution path is from {open_list[0][2][0]} to {goal} is {open_list[0][2]} with distance : {open_list[0][1]}")
+    print("something went wrong moslty the node not found : ( ")        
+print(f"So the optimized path is from {open_list[0][2][0]} to {goal_node} is {open_list[0][2]} with distance : {open_list[0][1]}")      
