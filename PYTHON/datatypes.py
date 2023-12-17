e9eb3e5ad8ef8ca15562fe82ad7c63668f5f4494
PrerakPatel13 <prerakpatel1301@gmail.com>
2023-12-17 16:06:29 +0530

    add

---
+++ PYTHON/datatypes.py
@@ -0,0 +1,56 @@
+a="Hello world"
+print("String:",a,type(a))
+
+print()
+
+b=10
+print("Integer:",b,type(b))
+
+print()
+
+com = 5+ 13j
+print("Complex:",com,type(com))
+
+print()
+
+list=['one',1,14+5j]
+print("List:",list,type(list))
+
+print()
+
+range =10
+print("Range:",range,type(range))
+
+print()
+
+tuple=('one',1,14+5j)
+print("Tuple:",tuple,type(tuple))
+
+print()
+
+dic={1:'hello', 2:'hi'}
+print('Dictionary:',dic,type(dic))
+print("Dictionary keys:",dic.keys())
+print("Dictionary values:",dic.values())
+print("Dictionary items:",dic.items())
+
+print()
+
+set={'one',1,14+5j}
+print("Set:",set,type(set))
+
+print()
+
+byte =b'hello'
+print("Byte:",byte,type(byte))
+
+print()
+
+n=None
+print("None type:",n,type(n))
+
+print()
+
+id1=123
+print("id of id1:",id(id1),"is of",id1)
+
